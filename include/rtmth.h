0aa6702936f7d80dbc8267783c1d82b34384cd9c
alraro <103150477+alraro@users.noreply.github.com>
2026-03-31 15:52:32 +0200

    feat: matrix member functions of rtmth library (#18)
    * feat: add matrix struct and identity matrix function with tests
    
    * feat: refactor matrix struct to mat4 and add matrix transposition function with tests. Optimized identity matrix function to be more efficient than loops.
    
    * feat: rename matrix functions to mat4 prefix and add determinant calculation with tests
    
    * feat: add matrix inverse function and corresponding tests
    
    * Fixed weird assignment for transposed matrix in mat4_inverse
    
    * Added cool colors to math tester
    
    * feat: add matrix multiplication function and corresponding tests
    
    * feat: add matrix translation function and update multiplication function name
    
    * feat: add rotation matrix functions for x, y, and z axes with corresponding tests
    
    * feat: add scaling matrix function and corresponding tests

--- include/rtmth.h
+++ include/rtmth.h
@@ -6,12 +6,13 @@
 /*   By: aramos-r <aramos-r@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/19 17:24:57 by sscheini          #+#    #+#             */
-/*   Updated: 2026/03/29 17:54:25 by aramos-r         ###   ########.fr       */
+/*   Updated: 2026/03/30 17:52:31 by aramos-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef RTMTH_H
 # define RTMTH_H
+# define EPSILON 0.00001
 
 # include <math.h>
 
@@ -21,6 +22,16 @@ typedef struct s_vector
 	double	y;
 	double	z;
 }	t_vector;
+
+typedef struct s_mat4
+{
+	double	m[4][4];
+}	t_mat4;
+
+typedef struct s_mat3
+{
+	double	m[3][3];
+}	t_mat3;
 
 /**
  * @brief Creates a new vector with the given x, y, and z components.
@@ -110,5 +121,118 @@ t_vector	vector_cross_product(t_vector v1, t_vector v2);
  * @return The distance between the two points as a double.
  */
 double		vector_distance_points(t_vector p1, t_vector p2);
+
+/**
+ * @brief Creates a new identity matrix.
+ * @return The newly created identity matrix.
+ * @note An identity matrix is a square matrix with ones on the main diagonal
+ * and zeros elsewhere.
+ */
+t_mat4		mat4_new_identity(void);
+
+/**
+ * @brief Creates a new matrix which is the transposed version of the given one.
+ * @param mat The matrix to transpose.
+ * @return The transposed matrix.
+ */
+t_mat4		mat4_transposed(t_mat4 mat);
+
+/**
+ * @brief Calculates the determinant of a 4x4 matrix.
+ * @param mat The matrix for which to calculate the determinant.
+ * @return The determinant of the matrix as a double.
+ */
+double		mat4_determinant(t_mat4 mat);
+
+/**
+ * @brief Creates a new matrix which is the inverse of the given one.
+ * @param mat The matrix to invert.
+ * @return The inverted matrix.
+ * @warning If the matrix is not invertible (i.e., its determinant is 0),
+ * the function will return the identity matrix.
+ */
+t_mat4		mat4_inverse(t_mat4 mat);
+
+/**
+ * @brief Multiplies two 4x4 matrices together.
+ * @param m1 The first matrix to be multiplied.
+ * @param m2 The second matrix to be multiplied.
+ * @return The resulting matrix after multiplication.
+ * @note Matrix multiplication is not commutative,
+ * meaning that mat4_mult_mat4(m1, m2)
+ * may not equal mat4_mult_mat4(m2, m1).
+ */
+t_mat4		mat4_mult_mat4(t_mat4 m1, t_mat4 m2);
+
+/**
+ * @brief Creates a new translation matrix for the given translation values.
+ * This matrix can be used to translate points in 3D space by the specified
+ * amounts along the x, y, and z axes.
+ * @param tx The translation amount along the x-axis, should be a double.
+ * @param ty The translation amount along the y-axis, should be a double.
+ * @param tz The translation amount along the z-axis, should be a double.
+ * @return The newly created translation matrix.
+ * @note The resulting matrix will have the form:
+ * @note [ 1 0 0 tx ]
+ * @note [ 0 1 0 ty ]
+ * @note [ 0 0 1 tz ]
+ * @note [ 0 0 0 1 ]
+ */
+t_mat4		mat4_translation(double tx, double ty, double tz);
+
+/**
+ * @brief Creates a new rotation matrix for a rotation
+ * around the x-axis by the given angle.
+ * @param angle The angle of rotation in radians, should be a double.
+ * @return The newly created rotation matrix.
+ * @note The resulting matrix will have the form:
+ * @note [ 1 0 0 0 ]
+ * @note [ 0 cos(angle) -sin(angle) 0 ]
+ * @note [ 0 sin(angle) cos(angle) 0 ]
+ * @note [ 0 0 0 1 ]
+ */
+t_mat4		mat4_rotation_x(double angle);
+
+/**
+ * @brief Creates a new rotation matrix for a rotation
+ * around the y-axis by the given angle.
+ * @param angle The angle of rotation in radians, should be a double.
+ * @return The newly created rotation matrix.
+ * @note The resulting matrix will have the form:
+ * @note [ cos(angle) 0 sin(angle) 0 ]
+ * @note [ 0 1 0 0 ]
+ * @note [ -sin(angle) 0 cos(angle) 0 ]
+ * @note [ 0 0 0 1 ]
+ */
+t_mat4		mat4_rotation_y(double angle);
+
+/**
+ * @brief Creates a new rotation matrix for a rotation
+ * around the z-axis by the given angle.
+ * @param angle The angle of rotation in radians, should be a double.
+ * @return The newly created rotation matrix.
+ * @note The resulting matrix will have the form:
+ * @note [ cos(angle) -sin(angle) 0 0 ]
+ * @note [ sin(angle) cos(angle) 0 0 ]
+ * @note [ 0 0 1 0 ]
+ * @note [ 0 0 0 1 ]
+ */
+t_mat4		mat4_rotation_z(double angle);
+
+/**
+ * @brief Creates a new scaling matrix for the given scaling factors.
+ * This matrix can be used to scale points in 3D space by the specified
+ * amounts along the x, y, and z axes.
+ * @param sx The scaling factor along the x-axis, should be a double.
+ * @param sy The scaling factor along the y-axis, should be a double.
+ * @param sz The scaling factor along the z-axis, should be a double.
+ * @return The newly created scaling matrix.
+ * @note The resulting matrix will have the form:
+ * @note [ sx 0 0 0 ]
+ * @note [ 0 sy 0 0 ]
+ * @note [ 0 0 sz 0 ]
+ * @note [ 0 0 0 1 ]
+ */
+t_mat4		mat4_scale(double sx, double sy, double sz);
 
 #endif
