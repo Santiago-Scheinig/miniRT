d844dd9cf4482c228feb3c0a08a511ee882e6537
alraro <alfonsoramosrojas@gmail.com>
2026-04-01 20:28:41 +0200

    Implemneted function for inverse matrix of sphere

--- src/libs/librt/src/rtelm/sphere.c
+++ src/libs/librt/src/rtelm/sphere.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   sphere.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sscheini <sscheini@student.42malaga.com    +#+  +:+       +#+        */
+/*   By: aramos-r <aramos-r@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/24 19:58:15 by sscheini          #+#    #+#             */
-/*   Updated: 2026/04/01 17:37:21 by sscheini         ###   ########.fr       */
+/*   Updated: 2026/04/01 20:27:38 by aramos-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,4 +16,16 @@ t_object	*new_sphere(char **str)
 {
 	(void) str;
 	return (NULL);
+}
+
+t_mat4	sphere_get_inverse_mat4(t_vector center, double diameter)
+{
+	t_mat4	inv;
+	t_mat4	trans;
+	t_mat4	scale;
+	
+	trans = mat4_translation(center.x, center.y, center.z);
+	scale = mat4_scale(diameter / 2.0, diameter / 2.0, diameter / 2.0);
+	inv = mat4_inverse(mat4_mult_mat4(trans, scale));
+	return (inv);
 }
