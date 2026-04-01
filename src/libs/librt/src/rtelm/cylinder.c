894e59f6257d0d787abdef8c3cb2bfcd6eeae2e7
alraro <alfonsoramosrojas@gmail.com>
2026-04-01 20:31:43 +0200

    Implemented function for inveres matrix in cylinder

--- src/libs/librt/src/rtelm/cylinder.c
+++ src/libs/librt/src/rtelm/cylinder.c
@@ -6,7 +6,7 @@
 /*   By: aramos-r <aramos-r@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/24 20:21:46 by sscheini          #+#    #+#             */
-/*   Updated: 2026/04/01 19:26:27 by aramos-r         ###   ########.fr       */
+/*   Updated: 2026/04/01 20:29:58 by aramos-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,3 +18,19 @@ t_object	*new_cylinder(char **str)
 	return (NULL);
 }
 
+t_mat4	cylinder_get_inverse_mat4(t_vector center,
+					double diameter,
+					t_vector normal,
+					double height)
+{
+	t_mat4	inv;
+	t_mat4	trans;
+	t_mat4	rot;
+	t_mat4	scale;
+	
+	trans = mat4_translation(center.x, center.y, center.z);
+	scale = mat4_scale(diameter / 2.0, height / 2.0, diameter / 2.0);
+	rot = mat4_rotation(normal);
+	inv = mat4_inverse(mat4_mult_mat4(trans, mat4_mult_mat4(rot, scale)));
+	return (inv);
+}
