d62782ce75f3c1d310cd77279626024eb3dffd52
Santiago <santiago.scheinig@gmail.com>
2026-04-01 17:48:47 +0200

    feat: vector check for invalid 0,0,0 vectors added

--- src/libs/librt/src/rtapp/rtapp_init/parser/parse_utils.c
+++ src/libs/librt/src/rtapp/rtapp_init/parser/parse_utils.c
@@ -6,7 +6,7 @@
 /*   By: sscheini <sscheini@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/25 19:13:33 by sscheini          #+#    #+#             */
-/*   Updated: 2026/04/01 17:37:05 by sscheini         ###   ########.fr       */
+/*   Updated: 2026/04/01 17:48:19 by sscheini         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -70,37 +70,50 @@ int	parse_float(char *sp, char *str, int i, t_flim limits)
 	return (ans);
 }
 
-static int	vector_checl(char *sp, t_vector v, int line)
+static int	vector_check(char *sp, char **split, int line, t_flim limits)
 {
-	
+	const char	*errmsg = "Parser error for %s in line %i: %s";
+	t_vector	v;
+	int			ans;
+
+	ans = RT_SUCCESS;
+	if (parse_float(sp, split[0], line, limits))
+		ans = RT_FAILURE;
+	else if (parse_float(sp, split[1], line, limits))
+		ans = RT_FAILURE;
+	else if (parse_float(sp, split[2], line, limits))
+		ans = RT_FAILURE;
+	else if (split[3])
+	{
+		rtlog(RT_ERRLOG, 0, errmsg, sp, line, "invalid 3D vector.");
+		ans = RT_FAILURE;
+	}
+	v.x = atof(split[0]);//Need to change to ours
+	v.y = atof(split[1]);//Need to change to ours
+	v.z = atof(split[2]);//Need to change to ours
+	if (!v.x && !v.y && !v.z)
+		ans = RT_FAILURE;
+	return (ans);
 }
 
 int parse_vector(char *sp, char *str, int i, t_flim limits)
 {
 	const char	*errmsg = "Parser error for %s in line %i: %s";
 	char		**split;
-	int			ans;
 	
-	ans = RT_SUCCESS;
 	split = ft_split(str, ',');
 	if (!split)
 	{
 		rtlog(RT_ERRLOG, 0, errmsg, sp, i, strerror(errno));	
 		return (RT_FAILURE);
 	}
-	if (parse_float(sp, split[0], i, limits))
-		ans = RT_FAILURE;
-	else if (parse_float(sp, split[1], i, limits))
-		ans = RT_FAILURE;
-	else if (parse_float(sp, split[2], i, limits))
-		ans = RT_FAILURE;
-	else if (split[3])
+	if (vector_check(sp, split, i, limits))
 	{
-		rtlog(RT_ERRLOG, 0, errmsg, sp, i, "invalid 3D vector.");
-		ans = RT_FAILURE;
+		ft_split_free(split);
+		return (RT_FAILURE);
 	}
 	ft_split_free(split);
-	return (ans);
+	return (RT_SUCCESS);
 }
 
 int	parse_color(char *sp, char *str, int i)
