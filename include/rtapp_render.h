003dac3b74c6c488914776588e579858f4bc16e1
alraro <alfonsoramosrojas@gmail.com>
2026-04-21 18:02:01 +0200

    Empezado get color at hit

--- include/rtapp_render.h
+++ include/rtapp_render.h
@@ -6,7 +6,7 @@
 /*   By: aramos-r <aramos-r@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/04 17:56:49 by aramos-r          #+#    #+#             */
-/*   Updated: 2026/04/21 17:29:02 by aramos-r         ###   ########.fr       */
+/*   Updated: 2026/04/21 17:58:11 by aramos-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -97,6 +97,23 @@ int				get_next_tile(t_tile_queue *queue, t_tile *tile);
  * to INFINITY.
  */
 t_hit			get_hit_from_ray(t_ray ray, t_object *objects);
+
+/**
+ * @brief Computes the color at a ray-object intersection point using the
+ * Phong reflection model, accounting for all light sources in the scene.
+ * @param hit The T_HIT containing the intersection information for shading.
+ * @param objects The array of T_OBJECTs in the scene for shadow ray testing.
+ * @param lights The array of T_ELEM_LIGHT_P point lights in the scene.
+ * @param alight The T_ELEM_LIGHT_A ambient light in the scene.
+ * @return A T_VECTOR representing the final RGB color at the hit point, with
+ * each component normalized to the 0.0-1.0 range.
+ */
+t_vector		get_color_at_hit(
+					t_hit hit,
+					t_object *objs,
+					t_elem_light_p *lights,
+					t_elem_light_a *alight
+				);
 
 /*--------------------------------------------------------------------------*/
 /*------------------------------------END-----------------------------------*/
